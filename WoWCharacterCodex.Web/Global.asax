c0604dcd1a42dc7c30a66703f9fc84cfe4d2f6d8
Charles Hunter <charles082986@gmail.com>
2019-01-17 12:09:29 -0600

    Add project files.

---
+++ WoWCharacterCodex.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WoWCharacterCodex.Web.MvcApplication" Language="C#" %>
---
+++ WoWCharacterCodex.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WoWCharacterCodex.Web.Startup))]
+namespace WoWCharacterCodex.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
